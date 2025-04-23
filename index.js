2db79fd724191c0f93b8701c174fa9e38e3dd937
kahn-arwen <arwenkaren1207@gmail.com>
2025-04-22 22:06:57 -0300

    atualização

---
+++ index.js
@@ -0,0 +1,28 @@
+{
+  "expo": {
+    "name": "takealook",
+    "slug": "snack-ef77f458-bd25-46ce-aef9-8f9d70282110",
+    "version": "1.0.0",
+    "orientation": "portrait",
+    "icon": "./assets/icon.png",
+    "userInterfaceStyle": "light",
+    "newArchEnabled": true,
+    "splash": {
+      "image": "./assets/splash-icon.png",
+      "resizeMode": "contain",
+      "backgroundColor": "#ffffff"
+    },
+    "ios": {
+      "supportsTablet": true
+    },
+    "android": {
+      "adaptiveIcon": {
+        "foregroundImage": "./assets/adaptive-icon.png",
+        "backgroundColor": "#ffffff"
+      }
+    },
+    "web": {
+      "favicon": "./assets/favicon.png"
+    }
+  }
+}
